8dbd07d48ecf40b9c298c8a0ddeeafcda642bb4d
rohan27hadvani <rohan.hadvani0@gmail.com>
2024-09-26 01:23:55 +0530

    Title and Para Element Designed

--- portfolio-1/src/App.jsx
+++ portfolio-1/src/App.jsx
@@ -21,7 +21,7 @@ function App() {
             </div>
             </div>
             <div>
-            <li className='text-black flex gap-16 font-gilroy uppercase text-2xl justify-end pr-10 w-full'>
+            <li className='text-black flex gap-16 font-gilroy uppercase text-2xl justify-end pr-8 w-full'>
             <ul className='font-light'>
               Projects
             </ul>
@@ -34,10 +34,44 @@ function App() {
             </li>
           </div>
           </div>
-          <div className="box-1 row-span-4 col-span-4 basic">2</div>
-          <div className="box-1 row-span-4 col-span-2 basic">3</div>
-          <div className="box-1 row-span-6 col-span-3 basic">4</div>
-          <div className="box-1 row-span-3 col-span-3 basic">5</div>
+
+
+
+
+          <div className="box-1 row-span-4 col-span-4 basic">
+            <div className='text-black font-gilroy text-6xl w-full pl-8 h-1/5'>
+              <div className='font-bold'>
+                Full-Stack Developer
+              </div> 
+              <div className='font-bold'>
+                Thriving Author
+              </div> 
+              <div>
+                Sports Enthusiast
+              </div> 
+              <div className='font-bold'>
+                College Drop-out
+              </div> 
+            </div>
+          </div>
+          
+          
+          
+          <div className="box-1 row-span-4 col-span-2 basic">
+            3
+          </div>
+
+
+          <div className="box-1 row-span-6 col-span-3 basic">
+            4
+          </div>
+          
+          
+          <div className="box-1 row-span-3 col-span-3 basic"> 
+            <div className='text-black font-gilroy w-full h-1 pl-8 pr-10  font-light text-2xl'>
+              Julia Huang is an innovative AI artist, renowned for blending cutting-edge technology with creative expression. Based in LA, she crafts unique digital art experiences accessible globally.
+            </div>
+          </div>
           <div className="bg-cream row-span-3 col-span-3 basic">6</div>
           <div className="box-1 row-span-1 col-span-3 basic">7</div>
         </div>
