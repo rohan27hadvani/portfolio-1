fadd02a01691637a77b2a8be6f7d06d93e28df12
rohan27hadvani <rohan.hadvani0@gmail.com>
2024-09-26 18:30:38 +0530

    Animations without delay

--- portfolio-1/src/App.jsx
+++ portfolio-1/src/App.jsx
@@ -5,19 +5,52 @@ import './fonts.css'
 import './animation.css'
 
 function App() {
-  // const [div1, setDiv1] = useState(false);
-  // const [div2, setDiv2] = useState(false);
+  const [div1, setDiv1] = useState(false);
+  const [div2, setDiv2] = useState(false);
   const [div3, setDiv3] = useState(false);
-  // const [div4, setDiv4] = useState(false);
-  // const [div5, setDiv5] = useState(false);
-  // const [div6, setDiv6] = useState(false);
-  // const [div7, setDiv7] = useState(false);
+  const [div4, setDiv4] = useState(false);
+  const [div5, setDiv5] = useState(false);
+  const [div6, setDiv6] = useState(false);
+  const [div7, setDiv7] = useState(false);
 
   useEffect(() => {
-    const timer = setTimeout(() => {
+    const timer1 = setTimeout(() => {
+      setDiv1(true);
+    }, 100);
+
+    const timer2 = setTimeout(() => {
+      setDiv2(true);
+    }, 100);
+
+    const timer3 = setTimeout(() => {
       setDiv3(true);
-    }, 100); // Adjust this delay as needed
-    return () => clearTimeout(timer);
+    }, 100);
+
+    const timer4 = setTimeout(() => {
+      setDiv4(true);
+    }, 100);
+
+    const timer5 = setTimeout(() => {
+      setDiv5(true);
+    }, 100);
+    
+    const timer6 = setTimeout(() => {
+      setDiv6(true);
+    }, 100);
+
+    const timer7 = setTimeout(() => {
+      setDiv7(true);
+    }, 100);
+
+    return () => {
+      clearTimeout(timer1);
+      clearTimeout(timer2);
+      clearTimeout(timer3);
+      clearTimeout(timer4);
+      clearTimeout(timer5);
+      clearTimeout(timer6);
+      clearTimeout(timer7);
+    };
   }, []);
 
   return (
@@ -25,7 +58,8 @@ function App() {
       <div className="h-screen w-screen flex items-center justify-center p-4 bg-lightbg">
         <div className="relative w-full h-full ">
           <div className="grid grid-rows-8 grid-cols-9 h-full w-full gap-4">
-            <div className="box-1 row-span-1 col-span-9 basic">
+          
+            <div className={`box-1 row-span-1 col-span-9 basic ${div1 ? 'moving-1' : ''}`}>
               <div className='text-black flex gap-2 font-gilroy uppercase text-2xl justify-start pl-8 w-full'>
                 <div className='italic'>
                   Rohan
@@ -48,8 +82,9 @@ function App() {
                 </li>
               </div>
             </div>
+            
 
-            <div className="box-1 row-span-4 col-span-4 basic">
+            <div className={`box-1 row-span-4 col-span-4 basic ${div2 ? 'moving-2' : ''}`}>
               <div className='text-black font-gilroy text-6xl w-full pl-8 h-1/5'>
                 <div className='font-bold'>
                   Full-Stack Developer
@@ -66,15 +101,15 @@ function App() {
               </div>
             </div>
 
-            <div className={`box-1 row-span-4 col-span-2 basic ${div3 ? 'moving' : ''}`}>
+            <div className={`box-1 row-span-4 col-span-2 basic ${div3 ? 'moving-3' : ''}`}>
               3
             </div>
 
-            <div className="box-1 row-span-6 col-span-3 basic">
+            <div className={`box-1 row-span-6 col-span-3 basic ${div4 ? 'moving-4' : ''}`}>
               4
             </div>
 
-            <div className="box-1 row-span-3 col-span-3 basic"> 
+            <div className={`box-1 row-span-3 col-span-3 basic ${div5 ? 'moving-5' : ''}`}> 
               <div className='text-black font-gilroy w-full h-1 pl-8 pr-10 font-light text-2xl'>
                 <section>
                   Julia Huang is an innovative AI artist, renowned for blending cutting-edge technology with creative expression. Based in LA, she crafts unique digital art experiences accessible globally.
@@ -82,11 +117,11 @@ function App() {
               </div>
             </div>
 
-            <div className="bg-cream row-span-3 col-span-3 basic">
+            <div className={`bg-cream row-span-3 col-span-3 basic ${div6 ? 'moving-6' : ''}`}>
               6
             </div>
 
-            <div className="box-1 row-span-1 col-span-3 basic">
+            <div className={`box-1 row-span-1 col-span-3 basic ${div7 ? 'moving-7' : ''}`}>
               <div>
                 <li className='text-black flex gap-16 font-gilroy uppercase text-2xl w-full'>
                   <ul className='font-light hover:underline'>
