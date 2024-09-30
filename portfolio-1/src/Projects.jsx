7531a3e3232dc5e088fb84550149d9fe2e3cf67a
rohan27hadvani <rohan.hadvani0@gmail.com>
2024-09-30 19:00:12 +0530

    Projects Animation Bug Fixed

--- portfolio-1/src/Projects.jsx
+++ portfolio-1/src/Projects.jsx
@@ -8,6 +8,7 @@ import './animation.css';
 
 function Projects() {
   const [load, setLoad] = useState(false);
+  const [isContentVisible, setIsContentVisible] = useState(false);
   const [theme] = useState(() => {
     // Retrieve the theme from localStorage
     return localStorage.getItem('selectedTheme') || 'theme1';
@@ -18,8 +19,13 @@ function Projects() {
       setLoad(true);
     }, 100);
 
+    const timeout = setTimeout(() => {
+      setIsContentVisible(true);
+    }, 300);
+
     return () => {
       clearTimeout(timer);
+      clearTimeout(timeout)
     };
   }, []);
 
@@ -45,112 +51,95 @@ function Projects() {
     <div className={`h-screen w-screen p-4 ${themeClasses[theme].bg}`}>
       <div className="w-full h-full">
         <div className="grid grid-rows-8 grid-cols-9 h-full w-full gap-4">
+            <Navbar theme={themeClasses[theme]} load={load} animationClass="moving-pn" />
+
+
+            <div className={`${themeClasses[theme].light} row-span-3 col-span-5 basic ${load ? 'moving-p1' : ''}
+              transition-opacity duration-1000 ease ${
+                isContentVisible ? 'opacity-100' : 'opacity-0'}`}>
+              <div className='text-black font-gilroy w-full h-full'>
+                <div className='flex w-full h-2/3'>
+                  <div className='w-2/5 text-xl pl-6 pt-8'>
+                    Have some questions?
+                  </div>
+                  <div className='w-3/5 pr-6 pt-8 flex justify-end'>
+                    <img src={carrow} alt="" className='h-12 hover:cursor-pointer hover:border-[0.5px] hover:border-black'/>
+                  </div>
+                </div>
+                <div className='h-1/3 text-5xl pl-6'>
+                  Contact me
+                </div>
+              </div>
+            </div>
+
+            <div className={`${themeClasses[theme].light} row-span-4 col-span-4 basic ${load ? 'moving-p2' : ''} transition-opacity duration-1000 ease ${
+                isContentVisible ? 'opacity-100' : 'opacity-0'}`}>
+              <div className='text-black font-gilroy w-full h-full'>
+                <div className='flex w-full h-2/3'>
+                  <div className='w-2/5 text-xl pl-6 pt-8'>
+                    Have some questions?
+                  </div>
+                  <div className='w-3/5 pr-6 pt-8 flex justify-end'>
+                    <img src={carrow} alt="" className='h-12 hover:cursor-pointer hover:border-[0.5px] hover:border-black'/>
+                  </div>
+                </div>
+                <div className='h-1/3 text-5xl pl-6'>
+                  Contact me
+                </div>
+              </div>
+            </div>
+
+            <div className={`${themeClasses[theme].light} row-span-4 col-span-2 basic ${load ? 'moving-p3' : ''} transition-opacity duration-1000 ease ${
+                isContentVisible ? 'opacity-100' : 'opacity-0'}`}>
+              <div className='text-black font-gilroy w-full h-full'>
+                <div className='flex w-full h-2/3'>
+                  <div className='w-2/5 text-xl pl-6 pt-8'>
+                    Have some questions?
+                  </div>
+                  <div className='w-3/5 pr-6 pt-8 flex justify-end'>
+                    <img src={carrow} alt="" className='h-12 hover:cursor-pointer hover:border-[0.5px] hover:border-black'/>
+                  </div>
+                </div>
+                <div className='h-1/3 text-5xl pl-6'>
+                  Contact me
+                </div>
+              </div>
+            </div>
           
-        <Navbar theme={themeClasses[theme]} load={load} animationClass="moving-pn" />
-
-
-          <div className={`${themeClasses[theme].light} row-span-3 col-span-5 basic ${load ? 'moving-p1' : ''}`}>
-            <div className='text-black font-gilroy w-full h-full'>
-              <div className='flex w-full h-2/3'>
-                <div className='w-2/5 text-xl pl-6 pt-8'>
-                  Have some questions?
+            <div className={`${themeClasses[theme].light} row-span-4 col-span-3 basic ${load ? 'moving-p4' : ''} transition-opacity duration-1000 ease ${
+                isContentVisible ? 'opacity-100' : 'opacity-0'}`}>
+              <div className='text-black font-gilroy w-full h-full'>
+                <div className='flex w-full h-2/3'>
+                  <div className='w-2/5 text-xl pl-6 pt-8'>
+                    Have some questions?
+                  </div>
+                  <div className='w-3/5 pr-6 pt-8 flex justify-end'>
+                    <img src={carrow} alt="" className='h-12 hover:cursor-pointer hover:border-[0.5px] hover:border-black'/>
+                  </div>
                 </div>
-                <div className='w-3/5 pr-6 pt-8 flex justify-end'>
-                  <img src={carrow} alt="" className='h-12 hover:cursor-pointer hover:border-[0.5px] hover:border-black'/>
+                <div className='h-1/3 text-5xl pl-6'>
+                  Contact me
                 </div>
-              </div>
-              <div className='h-1/3 text-5xl pl-6'>
-                Contact me
               </div>
             </div>
-          </div>
 
-          <div className={`${themeClasses[theme].light} row-span-4 col-span-4 basic ${load ? 'moving-p2' : ''}`}>
-            <div className='text-black font-gilroy w-full h-full'>
-              <div className='flex w-full h-2/3'>
-                <div className='w-2/5 text-xl pl-6 pt-8'>
-                  Have some questions?
+            <div className={`${themeClasses[theme].light} row-span-3 col-span-4 basic ${load ? 'moving-p5' : ''} transition-opacity duration-1000 ease ${
+                isContentVisible ? 'opacity-100' : 'opacity-0'}`}>
+              <div className='text-black font-gilroy w-full h-full'>
+                <div className='flex w-full h-2/3'>
+                  <div className='w-2/5 text-xl pl-6 pt-8'>
+                    Have some questions?
+                  </div>
+                  <div className='w-3/5 pr-6 pt-8 flex justify-end'>
+                    <img src={carrow} alt="" className='h-12 hover:cursor-pointer hover:border-[0.5px] hover:border-black'/>
+                  </div>
                 </div>
-                <div className='w-3/5 pr-6 pt-8 flex justify-end'>
-                  <img src={carrow} alt="" className='h-12 hover:cursor-pointer hover:border-[0.5px] hover:border-black'/>
+                <div className='h-1/3 text-5xl pl-6'>
+                  Contact me
                 </div>
-              </div>
-              <div className='h-1/3 text-5xl pl-6'>
-                Contact me
               </div>
             </div>
-          </div>
 
-          <div className={`${themeClasses[theme].light} row-span-4 col-span-2 basic ${load ? 'moving-p3' : ''}`}>
-            <div className='text-black font-gilroy w-full h-full'>
-              <div className='flex w-full h-2/3'>
-                <div className='w-2/5 text-xl pl-6 pt-8'>
-                  Have some questions?
-                </div>
-                <div className='w-3/5 pr-6 pt-8 flex justify-end'>
-                  <img src={carrow} alt="" className='h-12 hover:cursor-pointer hover:border-[0.5px] hover:border-black'/>
-                </div>
-              </div>
-              <div className='h-1/3 text-5xl pl-6'>
-                Contact me
-              </div>
-            </div>
-          </div>
-        
-          <div className={`${themeClasses[theme].light} row-span-4 col-span-3 basic ${load ? 'moving-p4' : ''}`}>
-            <div className='text-black font-gilroy w-full h-full'>
-              <div className='flex w-full h-2/3'>
-                <div className='w-2/5 text-xl pl-6 pt-8'>
-                  Have some questions?
-                </div>
-                <div className='w-3/5 pr-6 pt-8 flex justify-end'>
-                  <img src={carrow} alt="" className='h-12 hover:cursor-pointer hover:border-[0.5px] hover:border-black'/>
-                </div>
-              </div>
-              <div className='h-1/3 text-5xl pl-6'>
-                Contact me
-              </div>
-            </div>
-          </div>
-
-          <div className={`${themeClasses[theme].light} row-span-3 col-span-4 basic ${load ? 'moving-p5' : ''}`}>
-            <div className='text-black font-gilroy w-full h-full'>
-              <div className='flex w-full h-2/3'>
-                <div className='w-2/5 text-xl pl-6 pt-8'>
-                  Have some questions?
-                </div>
-                <div className='w-3/5 pr-6 pt-8 flex justify-end'>
-                  <img src={carrow} alt="" className='h-12 hover:cursor-pointer hover:border-[0.5px] hover:border-black'/>
-                </div>
-              </div>
-              <div className='h-1/3 text-5xl pl-6'>
-                Contact me
-              </div>
-            </div>
-          </div>
-
-          {/* <div className={`${themeClasses[theme].light} row-span-3 col-span-3 basic ${load ? 'moving-p6' : ''}`}>
-            <div className='text-black font-gilroy w-full h-full'>
-              <div className='flex w-full h-2/3'>
-                <div className='w-2/5 text-xl pl-6 pt-8'>
-                  Have some questions?
-                </div>
-                <div className='w-3/5 pr-6 pt-8 flex justify-end'>
-                  <img src={carrow} alt="" className='h-12 hover:cursor-pointer hover:border-[0.5px] hover:border-black'/>
-                </div>
-              </div>
-              <div className='h-1/3 text-5xl pl-6'>
-                Contact me
-              </div>
-            </div>
-          </div>
-         */}
-        
-        
-        
-        
-        
-        
         </div>
       </div>
     </div>
