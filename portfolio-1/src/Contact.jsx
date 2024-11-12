39d0da9969970dd7ed87a874eb126572a35040ba
rohan27hadvani <rohan.hadvani0@gmail.com>
2024-11-12 15:29:00 +0530

    Contact Page Toasts Added

--- portfolio-1/src/Contact.jsx
+++ portfolio-1/src/Contact.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
-// import carrow from './assets/icons/carrow.svg';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
 import './fonts.css';
 import './animation.css';
@@ -50,15 +51,64 @@ function Contact() {
     },
   };
 
+  const copyLinkToClipboard = () => {
+    const websiteURL = "https://rohanhadvani.xyz";
+    navigator.clipboard.writeText(websiteURL)
+      .then(() => {
+        toast.success("Link copied to clipboard!", {
+          position: "top-center",
+          autoClose: 1500,
+          hideProgressBar: true,
+          closeOnClick: true,
+          pauseOnHover: false,
+          draggable: true,
+        });
+      })
+      .catch((err) => {
+        console.error("Failed to copy link: ", err);
+        toast.error("Failed to copy the link", {
+          position: "top-center",
+          autoClose: 3000,
+          hideProgressBar: true,
+          closeOnClick: true,
+          pauseOnHover: false,
+          draggable: true,
+        });
+      });
+  };
+
+  const downloadResume = () => {
+    const resumeURL = "https://rohanhadvani.xyz";
+    window.open(resumeURL, '_blank')
+      .then(() => {
+        toast.success("Resume downloaded successfully!", {
+          position: "top-center",
+          autoClose: 1500,
+          hideProgressBar: true,
+          closeOnClick: true,
+          pauseOnHover: false,
+          draggable: true,
+        });
+      })
+      .catch((err) => {
+        console.error("Failed to download resume: ", err);
+        toast.error("Failed to download resume", {
+          position: "top-center",
+          autoClose: 3000,
+          hideProgressBar: true,
+          closeOnClick: true,
+          pauseOnHover: false,
+          draggable: true,
+        });
+      });
+  };
+
   return (
     <div className={`row-span-7 col-span-9 ${themeClasses[theme].bg}`}>
       <div className="w-full h-full grid grid-rows-7 grid-cols-9 gap-4 box-border
-      
       mobile:inline-block tablet:grid">
 
-
         <div className={`${themeClasses[theme].light} row-span-3 col-span-2 basic ${load ? 'moving-p1' : ''} transition-opacity duration-1000 ease ${isContentVisible ? 'opacity-100' : 'opacity-0'} order-1
-          
         mobile:mt-4 mobile:py-8 tablet:mt-0 tablet:py-0`}>
           <div className='text-black font-gilroy w-full h-full'>
             <div className='flex flex-col place-items-center text-center w-full h-full'>
@@ -66,74 +116,51 @@ function Contact() {
                 Download My Latest Resume!
               </div>
               <div className='h-2/5 flex items-center mobile:mt-10 tablet:mt-0'>
-                <a href="">
-                  <img src={Download} alt="Download" className='h-12 hover:cursor-pointer hover:border-[0.5px] hover:border-black' />
-                </a>
+                <img 
+                  src={Download} 
+                  alt="Download" 
+                  className='h-12 hover:cursor-pointer hover:border-[0.5px] hover:border-black' 
+                  onClick={downloadResume}
+                />
               </div>
             </div>
           </div>
         </div>
 
-
         <div className={`${themeClasses[theme].dark} row-span-1 col-span-5 basic ${load ? 'moving-p3' : ''} transition-opacity duration-1000 ease ${isContentVisible ? 'opacity-100' : 'opacity-0'} order-2
-        
         mobile:mt-4 mobile:p-4 tablet:mt-0 tablet:p-0`}>
           <div className='w-full h-full flex text-center items-center justify-center text-4xl font-gilroy text-black'>
             Feel Free To Contact Me!
           </div>
         </div>
 
-        
-
-        {/* <div className={`${themeClasses[theme].light} row-span-7 col-span-2 basic ${load ? 'moving-p5' : ''} transition-opacity duration-1000 ease ${isContentVisible ? 'opacity-100' : 'opacity-0'}`}>
-          <div className='text-black font-gilroy w-full h-full'>
-            <div className='flex w-full h-2/3'>
-              <div className='w-2/5 text-4xl pl-6 pt-8'>
-                Have some questions?
-              </div>
-              <div className='w-3/5 pr-6 pt-8 flex justify-end'>
-                <img src={carrow} alt="Arrow" className='h-12 hover:cursor-pointer hover:border-[0.5px] hover:border-black' />
-              </div>
-            </div>
-            <div className='h-1/3 text-2xl pl-6'>
-              Contact me
-            </div>
-          </div>
-        </div> */}
-
         <div className={`${themeClasses[theme].light} row-span-6 col-span-5 basic ${load ? 'moving-p4' : ''} transition-opacity duration-1000 ease ${isContentVisible ? 'opacity-100' : 'opacity-0'} order-4
-          
         mobile:mt-4 tablet:mt-0`}>
           <div className='text-black font-gilroy w-full h-full flex items-center justify-center'>
             <div className='flex flex-col justify-center h-full py-8'> 
-              <ul className='grid grid-cols-2 w-full items-center justify-items-center 
-              
+              <ul className='grid grid-cols-2 w-full items-center justify-items-center
               mobile:inline-block tablet:grid tablet:gap-y-[95px] tablet:gap-x-[95px] desktop:gap-y-[200px] desktop:gap-x-[200px]'>
                 <li className='flex justify-center items-center'>
                   <a href="mailto:rohan.hadvani0@gmail.com" target="_blank" rel="noopener noreferrer">
                     <img src={Email} alt="Email" className="h-[125px] w-[125px]
-                    
                     mobile:scale-75 tablet:scale-[0.7] desktop:scale-100" />
                   </a>
                 </li>
                 <li className='flex justify-center items-center'>
                   <a href="https://www.instagram.com/rohanhadvani/" target="_blank" rel="noopener noreferrer">
                     <img src={insta} alt="Instagram" className="h-[100px] w-[100px]
-                    
                     mobile:scale-75 tablet:scale-[0.7] desktop:scale-100" />
                   </a>
                 </li>
                 <li className='flex justify-center items-center'>
                   <a href="https://www.linkedin.com/in/rohan-hadvani-aa159b229/" target="_blank" rel="noopener noreferrer">
                     <img src={LI} alt="LinkedIn" className="h-[140px] w-[140px]
-                    
                     mobile:scale-75 tablet:scale-[0.7] desktop:scale-100" />
                   </a>
                 </li>
                 <li className='flex justify-center items-center font-light hover:underline'>
                   <a href="https://github.com/rohan27hadvani" target='_blank' rel="noopener noreferrer">
                     <img src={GH} alt="GitHub" className="h-[100px] w-[100px]
-                    
                     mobile:scale-75 tablet:scale-[0.7] desktop:scale-100" />
                   </a>
                 </li>
@@ -142,19 +169,19 @@ function Contact() {
           </div>
         </div>
 
-
         <div className={`${themeClasses[theme].dark} row-span-4 col-span-2 basic ${load ? 'moving-p2' : ''} transition-opacity duration-1000 ease ${isContentVisible ? 'opacity-100' : 'opacity-0'} order-5
-          
         mobile:mt-4 mobile:pb-8 tablet:mt-0 tablet:pb-0`}>
           <div className='text-black font-gilroy w-full h-full'>
             <div className='flex flex-col place-items-center text-center w-full h-full'>
               <div className='w-full h-1/4 pr-6 pt-8 flex justify-end mobile:justify-center tablet:justify-end'>
-                <a href="">
-                  <img src={share} alt="Share" className='h-12 hover:cursor-pointer hover:border-[0.5px] hover:border-black' />
-                </a>
+                <img 
+                  src={share} 
+                  alt="Share" 
+                  className='h-12 hover:cursor-pointer hover:border-[0.5px] hover:border-black' 
+                  onClick={copyLinkToClipboard}
+                />
               </div>
               <div className='w-3/4 h-3/4 text-4xl flex items-center
-              
               mobile:mt-8 tablet:mt-0'>
                 Share Portfolio with others!
               </div>
@@ -163,20 +190,18 @@ function Contact() {
         </div>
 
         <div className={`${themeClasses[theme].dark} row-span-7 col-span-2 basic ${load ? 'moving-p5' : ''} transition-opacity duration-1000 ease ${isContentVisible ? 'opacity-100' : 'opacity-0'} order-3
-          
         mobile:mt-4 mobile:py-8 tablet:mt-0 tablet:py-0`}>
           <div className='text-black font-gilroy w-full h-full'>
             <div className='flex w-full h-full place-items-center'>
               <div className='w-full px-5 text-center text-4xl'>
                 I do not know what to do with this space right now....
               </div>
-              
             </div>
-            
           </div>
         </div>
 
       </div> 
+      <ToastContainer />
     </div> 
   );
 }
